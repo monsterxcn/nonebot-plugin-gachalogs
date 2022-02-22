210aaabe949b39937e733d239b9dca9bb1e02351
monsterxcn <monsterxcn@gmail.com>
2022-02-22 13:02:59 +0800

    :zap:  给大家表演一个更新版本

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="nonebot-plugin-gachalogs",
-    version="0.1.0",
+    version="0.1.1",
     author="monsterxcn",
     author_email="monsterxcn@gmail.com",
     description="A Genshin GachaLogs analysis plugin for Nonebot2",
